b84dc14f2a89334435678b01d368c1176c447de8
Adriana <adriana.staudova@gmail.com>
2022-05-03 15:53:52 +0900

    User profiles and link cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
     @user = User.new(user_params)
     if @user.save 
       flash[:notice] = "Hi #{@user.username}, welcome to the blog."
-      redirect_to articles_path
+      redirect_to @user
     else
       render "new"
     end
@@ -21,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "User was updated successfully."
-      redirect_to articles_path
+      redirect_to @user
     else
       render "edit"
     end

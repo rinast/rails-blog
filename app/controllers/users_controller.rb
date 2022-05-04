f2fb7cbbcc54e829b04647cd5c14cf21064965e5
Adriana <adriana.staudova@gmail.com>
2022-05-04 11:41:36 +0900

    Allow users to delete and add admin role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -37,6 +37,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:notice] = "Account and all associated articles successfully deleted."
+    redirect_to root_path
+  end
+
   private
 
   def user_params
@@ -48,8 +55,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user 
-    if current_user != @user
-      flash[:error] = "You can only edit your own profile."
+    if current_user != @user && !current_user.admin?
+      flash[:error] = "You can only edit or delete your own profile."
       redirect_to @user
     end
   end
